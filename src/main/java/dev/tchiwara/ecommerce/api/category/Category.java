3954292cdaf0500c7771f9815382d980b8f85884
tchiwara <tinotendachiwara47@gmail.com>
2026-08-09 23:22:44 +0530

    Add Category entity with JPA/Lombok annotations

---
+++ src/main/java/dev/tchiwara/ecommerce/api/category/Category.java
@@ -0,0 +1,20 @@
+package dev.tchiwara.ecommerce.api.category;
+import lombok.*;
+import jakarta.persistence.*;
+
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Short id;
+
+    @Column(name="name")
+    private String name;
+}
