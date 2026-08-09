f0770abffa8b975676e2917e5479a07ac638fa2a
tchiwara <tinotendachiwara47@gmail.com>
2026-08-10 00:10:43 +0530

    Add CategoryRepository extending JpaRepository

---
+++ src/main/java/dev/tchiwara/ecommerce/api/category/CategoryRepository.java
@@ -0,0 +1,6 @@
+package dev.tchiwara.ecommerce.api.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category,Byte> {
+}
