33609c59232d5e9e7c4390443a715fc8e0cca09d
tchiwara <tinotendachiwara47@gmail.com>
2026-08-09 12:12:45 +0530

    Initial commit: Category, Product, User migrations with Flyway

---
+++ src/test/java/dev/tchiwara/ecommerce/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tchiwara.ecommerce.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
