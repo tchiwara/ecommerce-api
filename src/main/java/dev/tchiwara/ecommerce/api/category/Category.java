4e1bc538a50be91a1a0b1c17f9b3fa53f9bacd9a
tchiwara <tinotendachiwara47@gmail.com>
2026-08-09 23:34:40 +0530

    Change Category id from Short to Byte to accurately match TINYINT column range

--- src/main/java/dev/tchiwara/ecommerce/api/category/Category.java
+++ src/main/java/dev/tchiwara/ecommerce/api/category/Category.java
@@ -13,7 +13,7 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Short id;
+    private Byte id;
 
     @Column(name="name")
     private String name;
