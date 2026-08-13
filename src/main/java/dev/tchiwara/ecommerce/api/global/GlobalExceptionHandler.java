84d7c095ac33e79b406f872a3063a9c6c01a6550
tchiwara <tinotendachiwara47@gmail.com>
2026-08-14 01:26:13 +0530

    Add exception handler for ResourceNotFoundException, return 404

--- src/main/java/dev/tchiwara/ecommerce/api/global/GlobalExceptionHandler.java
+++ src/main/java/dev/tchiwara/ecommerce/api/global/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package dev.tchiwara.ecommerce.api.global;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -21,6 +22,15 @@ public class GlobalExceptionHandler {
         });
 
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Map<String,   String>> handleResourceNotFound(
+            ResourceNotFoundException exception
+    ){
+        var error=new HashMap<String, String>();
+        error.put("message",exception.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
 }
