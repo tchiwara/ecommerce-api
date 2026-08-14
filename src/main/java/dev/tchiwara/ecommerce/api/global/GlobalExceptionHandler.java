8a2a47662c5309bc55dafa919b1e00e5812cf00d
tchiwara <tinotendachiwara47@gmail.com>
2026-08-14 23:45:46 +0530

    Add MethodArgumentTypeMismatchException handler with param name and value

--- src/main/java/dev/tchiwara/ecommerce/api/global/GlobalExceptionHandler.java
+++ src/main/java/dev/tchiwara/ecommerce/api/global/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -31,6 +32,13 @@ public class GlobalExceptionHandler {
         var error=new HashMap<String, String>();
         error.put("message",exception.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<Map<String, String>> handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        var error = new HashMap<String, String>();
+        error.put("message", "Invalid value '" + ex.getValue() + "' for parameter: " + ex.getName());
+        return ResponseEntity.badRequest().body(error);
     }
 
 }
