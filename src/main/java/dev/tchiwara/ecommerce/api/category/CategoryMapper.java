5c45af87717c5e065e19fe050b46568743d70f74
tchiwara <tinotendachiwara47@gmail.com>
2026-08-16 14:42:06 +0530

    feat(category): add mapper method for update

--- src/main/java/dev/tchiwara/ecommerce/api/category/CategoryMapper.java
+++ src/main/java/dev/tchiwara/ecommerce/api/category/CategoryMapper.java
@@ -2,11 +2,13 @@ package dev.tchiwara.ecommerce.api.category;
 import dev.tchiwara.ecommerce.api.category.dtos.CategoryRequestDTO;
 import dev.tchiwara.ecommerce.api.category.dtos.CategoryResponseDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel="spring")
 public interface CategoryMapper {
 
     Category toEntity(CategoryRequestDTO categoryRequestDTO);
     CategoryResponseDTO toDTO(Category category);
+    void updateCategory(CategoryRequestDTO categoryRequestDTO, @MappingTarget Category category);
 
 }
