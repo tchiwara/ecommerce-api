c3a5754cf1d91d37a05b3c4be069e0400fdf4228
tchiwara <tinotendachiwara47@gmail.com>
2026-08-27 23:48:49 +0530

    feat: add Product entity

---
+++ src/main/java/dev/tchiwara/ecommerce/api/product/Product.java
@@ -0,0 +1,35 @@
+package dev.tchiwara.ecommerce.api.product;
+
+import dev.tchiwara.ecommerce.api.category.Category;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+}
