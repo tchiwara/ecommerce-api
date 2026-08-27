93ec8a95812249f711da9fd767bc19102a0e703d
tchiwara <tinotendachiwara47@gmail.com>
2026-08-27 23:52:46 +0530

    feat: add ProductRepository

---
+++ src/main/java/dev/tchiwara/ecommerce/api/product/ProductRepository.java
@@ -0,0 +1,6 @@
+package dev.tchiwara.ecommerce.api.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+}
