a9889f1fcca3f42f7e4a65a627b3acda3b653441
tchiwara <tinotendachiwara47@gmail.com>
2026-08-30 15:54:33 +0530

    feat: add getAllProducts controller endpoint

--- src/main/java/dev/tchiwara/ecommerce/api/product/ProductController.java
+++ src/main/java/dev/tchiwara/ecommerce/api/product/ProductController.java
@@ -1,14 +1,15 @@
 package dev.tchiwara.ecommerce.api.product;
 
+import dev.tchiwara.ecommerce.api.category.dtos.CategoryResponseDTO;
 import dev.tchiwara.ecommerce.api.product.dtos.ProductRequestDTO;
 import dev.tchiwara.ecommerce.api.product.dtos.ProductResponseDTO;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.web.PagedModel;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
@@ -30,5 +31,13 @@ public class ProductController {
                 .buildAndExpand(response.getId())
                 .toUri();
         return ResponseEntity.created(uri).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<PagedModel<ProductResponseDTO>> getAllProducts(
+            @RequestParam(defaultValue = "0")  @Min(0) int page
+    ){
+        Page<ProductResponseDTO> productPage=productService.getAllProducts(page);
+        return ResponseEntity.ok(new PagedModel<>(productPage));
     }
 }
