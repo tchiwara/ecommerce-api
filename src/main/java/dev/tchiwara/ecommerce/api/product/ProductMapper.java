60dca546d08705c99b8517bda9dbf125ae0dfb15
tchiwara <tinotendachiwara47@gmail.com>
2026-08-31 22:45:12 +0530

    feat(product): add updateProduct mapping method to ProductMapper, ignoring category (set manually in service)

--- src/main/java/dev/tchiwara/ecommerce/api/product/ProductMapper.java
+++ src/main/java/dev/tchiwara/ecommerce/api/product/ProductMapper.java
@@ -1,9 +1,12 @@
 package dev.tchiwara.ecommerce.api.product;
 
+import dev.tchiwara.ecommerce.api.category.Category;
+import dev.tchiwara.ecommerce.api.category.dtos.CategoryRequestDTO;
 import dev.tchiwara.ecommerce.api.product.dtos.ProductRequestDTO;
 import dev.tchiwara.ecommerce.api.product.dtos.ProductResponseDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel="spring")
 public interface ProductMapper {
@@ -13,4 +16,8 @@ public interface ProductMapper {
 
     @Mapping(source = "category.id", target = "categoryId")
     ProductResponseDTO toDTO(Product product);
+
+    @Mapping(target = "category", ignore = true)
+    void updateProduct(ProductRequestDTO productRequestDTO, @MappingTarget Product product);
+
 }
