bf1af2c63db9e1d59e0762e67fffd740e7afcaa5
tchiwara <tinotendachiwara47@gmail.com>
2026-09-04 10:31:08 +0530

    feat(user): add GET /users endpoint returning paginated users

--- src/main/java/dev/tchiwara/ecommerce/api/user/UserController.java
+++ src/main/java/dev/tchiwara/ecommerce/api/user/UserController.java
@@ -3,17 +3,19 @@ package dev.tchiwara.ecommerce.api.user;
 import dev.tchiwara.ecommerce.api.user.dtos.UserRegisterRequestDTO;
 import dev.tchiwara.ecommerce.api.user.dtos.UserResponseDTO;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.web.PagedModel;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
+@Validated
 public class UserController {
 
     private  final UserService userService;
@@ -31,6 +33,14 @@ public class UserController {
                 .toUri();
 
         return ResponseEntity.created(uri).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<PagedModel<UserResponseDTO>> getAllUsers(
+            @RequestParam(defaultValue = "0") @Min(0) int page
+    ){
+        Page<UserResponseDTO> userPage=userService.getAllUsers(page);
+        return ResponseEntity.ok(new PagedModel<>(userPage));
     }
 
 }
