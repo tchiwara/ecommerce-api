10c6e4027f1484ee471a974e7dc7ffe3e4997ae7
tchiwara <tinotendachiwara47@gmail.com>
2026-09-13 00:20:12 +0530

    renamed CartItemResponse to CartItemResponseDTO

---
+++ src/main/java/dev/tchiwara/ecommerce/api/cart/dtos/CartItemResponseDTO.java
@@ -0,0 +1,20 @@
+package dev.tchiwara.ecommerce.api.cart.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItemResponseDTO {
+
+    private Long productId;
+    private String productName;
+    private int quantity;
+    private BigDecimal unitPrice;
+    private BigDecimal subtotal;
+}
