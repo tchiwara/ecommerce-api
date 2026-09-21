8bf0063d999d20b919e65fc3c23c9da77d2c03b4
tchiwara <tinotendachiwara47@gmail.com>
2026-09-21 23:55:40 +0530

    feat: add JwtService for JWT generation and validation

---
+++ src/main/java/dev/tchiwara/ecommerce/api/auth/JwtService.java
@@ -0,0 +1,49 @@
+package dev.tchiwara.ecommerce.api.auth;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${spring.jwt.secret}")
+    private String secret;
+
+    public String generateToken(String email){
+        final long tokenExpiration=86400;
+
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis()+ 1000 *tokenExpiration))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .compact();
+    }
+
+    public boolean validateToken(String token){
+
+        try{
+            var claims = getClaims(token);
+            return claims.getExpiration().after(new Date());
+        }
+        catch(JwtException ex){
+            return false;
+        }
+    }
+
+    private Claims getClaims(String token) {
+
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+}
